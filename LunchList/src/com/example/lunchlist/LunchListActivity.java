20bcf23de3f6df1f1ca73858e25110cb25c6c30d
Ryan Beverly <rbeverly@mymail.mines.edu>
2012-09-02 13:26:16 -0600

    apt 2 complete

--- LunchList/src/com/example/lunchlist/LunchListActivity.java
+++ LunchList/src/com/example/lunchlist/LunchListActivity.java
@@ -2,19 +2,25 @@ package com.example.lunchlist;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchListActivity extends Activity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lunch_list);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
-        return true;
-    }
+	Restaurant r=new Restaurant();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lunch_list);
+		Button save=(Button)findViewById(R.id.save);
+		save.setOnClickListener(onSave);
+	}
+	private View.OnClickListener onSave=new View.OnClickListener() {
+		public void onClick(View v) {
+			EditText name=(EditText)findViewById(R.id.name);
+			EditText address=(EditText)findViewById(R.id.addr);
+			r.setName(name.getText().toString());
+			r.setAddress(address.getText().toString());
+		}
+	};
 }
