f359d59b30abc62f9471937e15fae2ac6d316fb2
Ryan Beverly <rbeverly@mymail.mines.edu>
2012-09-04 16:34:09 -0600

    apt 5 done

--- LunchList/src/com/example/lunchlist/LunchListActivity.java
+++ LunchList/src/com/example/lunchlist/LunchListActivity.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.TextView;
 
 public class LunchListActivity extends Activity {
 	List<Restaurant> model=new ArrayList<Restaurant>();
@@ -26,7 +29,9 @@ public class LunchListActivity extends Activity {
 		adapter=new RestaurantAdapter();
 		list.setAdapter(adapter);
 	}
+	
 	private View.OnClickListener onSave=new View.OnClickListener() {
+		
 		public void onClick(View v) {
 			Restaurant r=new Restaurant();
 			EditText name=(EditText)findViewById(R.id.name);
@@ -34,6 +39,7 @@ public class LunchListActivity extends Activity {
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
 			RadioGroup types=(RadioGroup)findViewById(R.id.types);
+			
 			switch (types.getCheckedRadioButtonId()) {
 			case R.id.sit_down:
 				r.setType("sit_down");
@@ -48,11 +54,52 @@ public class LunchListActivity extends Activity {
 			adapter.add(r);
 		}
 	};
+
 	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
 		RestaurantAdapter() {
-			super(LunchListActivity.this,
-					android.R.layout.simple_list_item_1,
-					model);
+			super(LunchListActivity.this, android.R.layout.simple_list_item_1, model);
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row=convertView;
+			RestaurantHolder holder=null;
+			if (row==null) {
+				LayoutInflater inflater=getLayoutInflater();
+				row=inflater.inflate(R.layout.row, parent, false);
+				holder=new RestaurantHolder(row);
+				row.setTag(holder);
+			}
+			else {
+				holder=(RestaurantHolder)row.getTag();
+			}
+			holder.populateFrom(model.get(position));
+			return(row);
+		}
+	}
+
+	static class RestaurantHolder {
+		private TextView name=null;
+		private TextView address=null;
+		private ImageView icon=null;
+
+		RestaurantHolder(View row) {
+			name=(TextView)row.findViewById(R.id.title);
+			address=(TextView)row.findViewById(R.id.address);
+			icon=(ImageView)row.findViewById(R.id.icon);
+		}
+
+		void populateFrom(Restaurant r) {
+			name.setText(r.getName());
+			address.setText(r.getAddress());
+			if (r.getType().equals("sit_down")) {
+				icon.setImageResource(R.drawable.ball_red);
+			}
+			else if (r.getType().equals("take_out")) {
+				icon.setImageResource(R.drawable.ball_yellow);
+			}
+			else {
+				icon.setImageResource(R.drawable.ball_green);
+			}
 		}
 	}
 }
