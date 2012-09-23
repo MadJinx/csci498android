19eddbdd53bf4b18fc4b7716e008b4f5ef8d9ae3
Ryan Beverly <rbeverly@mymail.mines.edu>
2012-09-23 11:34:21 -0600

    apt 12 step 2 done

--- LunchList/src/com/example/lunchlist/LunchListActivity.java
+++ LunchList/src/com/example/lunchlist/LunchListActivity.java
@@ -10,6 +10,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.TabActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
@@ -156,23 +157,9 @@ public class LunchListActivity extends TabActivity {
 
 	private AdapterView.OnItemClickListener onListClick=new AdapterView.OnItemClickListener() {
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			
-			model.moveToPosition(position);
-			name.setText(helper.getName(model));
-			address.setText(helper.getAddress(model));
-			notes.setText(helper.getNotes(model));
-			
-			if (helper.getType(model).equals("sit_down")) {
-				types.check(R.id.sit_down);
-			}
-			else if (helper.getType(model).equals("take_out")) {
-				types.check(R.id.take_out);
-			}
-			else {
-				types.check(R.id.delivery);
-			}
-			
-			getTabHost().setCurrentTab(1);
+
+			Intent i=new Intent(LunchListActivity.this, DetailForm.class);
+			startActivity(i);
 		}
 	};
 
